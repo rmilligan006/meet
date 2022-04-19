e8ec6747b140ea025227cf53ea34f68eb047a35e
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-04-19 08:59:36 -0400

    added calendar events to the serverless and handler.js

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -50,7 +50,9 @@ module.exports.getAccessToken = async (event) => {
     client_secret,
     redirect_uris[0]
   );
-  const code = decodeURIComponent(`${event.pathParameters.code}`);
+  const code = event.pathParameters
+    ? decodeURIComponent(`${event.pathParameters.code}`)
+    : "";
 
   return new Promise((resolve, reject) => {
     oAuth2CLient.getToken(code, (err, token) => {
@@ -79,3 +81,54 @@ module.exports.getAccessToken = async (event) => {
       };
     });
 };
+
+module.exports.getCalendarEvents = event => {
+  const oAuth2CLient = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+  const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+    
+
+  oAuth2Client.setCredentials({ access_token });
+
+  return new Promise((resolve, reject ) => {
+    calendar.events.list(
+      {
+        calendarId: calendar_id,
+        auth: oAuth2Client,
+        timeMin: new Date().toISOString(),
+        singleEvents: true,
+        orderBy: "startTime",
+      },
+      (error, response) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      }
+    );
+
+  })
+  .then( results => {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify({ events: results.data.items })
+    };
+  })
+  .catch( error => {
+    return {
+      statusCode: 500,
+      headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      body: JSON.stringify(error),
+    };
+  });
+
+}
