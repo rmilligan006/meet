0da6d2cadb8e795f6c1fecffa09507fe2da5f03b
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-04-20 09:03:38 -0400

    Added testing, Event, and NumberOfEvents

--- src/__tests__/App.tests.js
+++ src/__tests__/App.tests.js
@@ -3,6 +3,8 @@ import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
 import CitySearch from '../CitySearch';
+import NumberOfEvents from '../NumberOfEvents';
+
 
 describe('<App /> component', () => {
   let AppWrapper;
@@ -17,4 +19,9 @@ describe('<App /> component', () => {
   test('render CitySearch', () => {
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
+
+ //test feature 2
+ test('render NumberOfEvents', () => {
+  expect(AppWrapper.find(NumberOfEvents)).toHaveLength(1);
+});
 });
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -11,7 +11,52 @@ describe("<EventList /> component", () => {
   });
 
   test("render an event", () => {
-    expect(EventWrapper.find(".event")).toHaveLength[1];
+    expect(EventWrapper.find(".event")).toHaveLength(1);
   });
  
+  test("render the location", () => {
+    expect(EventWrapper.find(".event")).toHaveLength(1);
+  })
+
+  test("render the summary", () => {
+    expect(EventWrapper.find(".summary")).toHaveLength(1);
+  })
+
+  //Button tests!
+
+  test("render the show details button", () => {
+    expect(EventWrapper.find(".show-details")).toHaveLength(1);
+  })
+
+  test("open details when the button is clicked", () => {
+    EventWrapper.setState({
+      collapsed: true,
+    });
+    EventWrapper.find(".show-details").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(false);
+  })
+
+  test("hide details when the button is clicked", () => {
+    EventWrapper.setState({
+      collapsed: false,
+    });
+    EventWrapper.find(".hide-details").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(true);
+  });
+
+  test("When the details button is clicked, the state will change from true to false", () => {
+    EventWrapper.setState({
+      collapsed: true,
+    });
+    EventWrapper.find(".details-button").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(false);
+  });
+
+  test("After the details button is clicked, the state will change from false to true", () => {
+    EventWrapper.setState({
+      collapsed: false,
+    });
+    EventWrapper.find(".details-button").simulate("click");
+    expect(EventWrapper.state("collapsed")).toBe(true);
+  })
 });
