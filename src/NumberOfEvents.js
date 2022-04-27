fd28677fc675b05ce6620133af9e186e509052d2
Ryan Milligan <90968645+rmilligan006@users.noreply.github.com>
2022-04-27 09:42:02 -0400

    Added alert events

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
 class NumberOfEvents extends Component {
   state = {
@@ -7,14 +8,25 @@ class NumberOfEvents extends Component {
 
   handleInputChange = (event) => {
     const value = event.target.value;
-    this.setState({ numberOfEvents: value });
-    this.props.updateEvents(null, value);
+
+    if (value <= 0 || value > 32) {
+      this.setState({
+        numberOfEvents: "",
+        infoText: "Please enter a number between 1 from 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: "",
+      });
+    }
   };
 
   render() {
     return (
       <div className="NumberOfEvents">
-        <p className="numbercount">Number of Events</p>
+        <ErrorAlert text={this.state.infoText} />
+
         <input
           className="number-of-events"
           type="number"
